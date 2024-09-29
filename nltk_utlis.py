0b451b5fc4c74fe0cca5bbb56120ecd5f7d75134
Steven <stevenmoussa03@gmail.com>
2024-09-29 22:05:13 +0100

    Implement and apply bag of words

--- nltk_utlis.py
+++ nltk_utlis.py
@@ -1,6 +1,8 @@
 import nltk
 nltk.download('punkt_tab')
 
+import numpy as np
+
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -11,4 +13,11 @@ def stem(word: str):
     return stemmer.stem(word.lower())
 
 def bag(tokenised_sentence, words):
-    pass
+    tokenised_sentence = [stem(w) for w in tokenised_sentence]
+    b = np.zeros(len(words), dtype=np.float32)
+
+    for i, w in enumerate(words):
+        if w in tokenised_sentence:
+            b[i] = 1.0
+
+    return b
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 from nltk_utlis import tokenise, stem, bag
 
 with open('intents.json', 'r') as f:
@@ -20,3 +21,16 @@ ignore_words = ['?', '.', ',','!']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
+
+X_train = []
+Y_train = []
+
+for (w, tag) in xy:
+    b = bag(w, all_words)
+    X_train.append(b)
+
+    label = tags.index(tag)
+    Y_train.append(label)
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
