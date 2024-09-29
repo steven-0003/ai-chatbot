1419d04383303d0d716e9ef8cce6ef663ff1bd4e
Steven <stevenmoussa03@gmail.com>
2024-09-29 22:05:39 +0100

    Create pytorch dataset

--- train.py
+++ train.py
@@ -2,6 +2,10 @@ import json
 import numpy as np
 from nltk_utlis import tokenise, stem, bag
 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
     
@@ -34,3 +38,22 @@ for (w, tag) in xy:
 
 X_train = np.array(X_train)
 Y_train = np.array(Y_train)
+
+class ChatDataSet(Dataset):
+    
+    def __init__(self) -> None:
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    def __getitem__(self, index) -> tuple:
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self) -> int:
+        return self.n_samples
+    
+#Hyperparameters
+batch_size = 8
+
+dataset = ChatDataSet()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
